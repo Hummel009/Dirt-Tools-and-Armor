3b55670f68ccdead155d7cf94dbe321ab29203d2
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-06 21:09:21 +0300

    17

--- appForgeArchive/1.17.1/src/main/java/dirtequipment/item/DTAItemArmor.java
+++ appForgeArchive/1.17.1/src/main/java/dirtequipment/item/DTAItemArmor.java
@@ -5,16 +5,15 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class DTAItemArmor extends ArmorItem {
-	public DTAItemArmor(EquipmentSlot slot) {
-		super(DTAArmorMaterials.DIRT, slot, new Properties().tab(CreativeModeTab.TAB_COMBAT));
+	public DTAItemArmor(EquipmentSlot equipmentSlot) {
+		super(DTAArmorMaterials.DIRT, equipmentSlot, new Properties().tab(CreativeModeTab.TAB_COMBAT));
 	}
 
 	@Override
-	public String getArmorTexture(ItemStack itemstack, Entity entity, EquipmentSlot slot, String layer) {
-		return "dirtequipment:textures/models/armor/dirt_layer_" + (slot == EquipmentSlot.LEGS ? 2 : 1) + ".png";
+	public String getArmorTexture(ItemStack itemStack, Entity entity, EquipmentSlot equipmentSlot, String layer) {
+		return "dirtequipment:textures/models/armor/dirt_layer_" + (equipmentSlot == EquipmentSlot.LEGS ? 2 : 1) + ".png";
 	}
 }
