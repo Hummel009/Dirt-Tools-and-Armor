7df9c5ab0b1316a357dd4c5e0369ddaaf6952cd3
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-18 21:34:01 +0300

    Update Main.java

--- appFabric/src/main/java/com/github/hummel/dirtequipment/Main.java
+++ appFabric/src/main/java/com/github/hummel/dirtequipment/Main.java
@@ -4,7 +4,6 @@ import com.github.hummel.dirtequipment.handler.FabricEventHandler;
 import com.github.hummel.dirtequipment.init.Items;
 import net.fabricmc.api.ModInitializer;
 
-@SuppressWarnings("WeakerAccess")
 public class Main implements ModInitializer {
 	public static final String DISABLE_CURSEFORGE_DUPLICATE_NOTICE = "131829122023";
 
