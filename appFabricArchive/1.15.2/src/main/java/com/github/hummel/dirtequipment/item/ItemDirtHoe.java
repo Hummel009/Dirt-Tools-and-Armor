8b7873383f1b360c1a23676f01ee680ce8ecbe82
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-19 13:10:55 +0300

    small fixes

--- appFabricArchive/1.15.2/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtHoe.java
+++ appFabricArchive/1.15.2/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtHoe.java
@@ -6,6 +6,6 @@ import net.minecraft.world.item.HoeItem;
 
 public class ItemDirtHoe extends HoeItem {
 	public ItemDirtHoe() {
-		super(ToolMaterials.DIRT, -3, new Properties().tab(CreativeModeTab.TAB_TOOLS));
+		super(ToolMaterials.DIRT, 0.0f, new Properties().tab(CreativeModeTab.TAB_TOOLS));
 	}
 }
