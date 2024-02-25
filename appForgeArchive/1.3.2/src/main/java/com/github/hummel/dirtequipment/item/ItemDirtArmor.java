f0209c92af20320de0794fc684cc501f30436efe
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-25 22:11:57 +0300

    armor texture fix

--- appForgeArchive/1.3.2/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtArmor.java
+++ appForgeArchive/1.3.2/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtArmor.java
@@ -14,6 +14,6 @@ public class ItemDirtArmor extends ItemArmor implements IArmorTextureProvider {
 
 	@Override
 	public String getArmorTextureFile(ItemStack itemStack) {
-		return "dirtequipment:textures/models/armor/dirt_layer_" + (armorType == 2 ? 2 : 1) + ".png";
+		return "/assets/dirtequipment/textures/models/armor/dirt_layer_" + (armorType == 2 ? 2 : 1) + ".png";
 	}
 }
