ed776177f4d417da726bf70d7dec4aa854628344
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-25 23:54:01 +0300

    fix

--- appFabricArchive/1.19.4/src/main/java/com/github/hummel/dirtequipment/handler/FabricEventHandler.java
+++ appFabricArchive/1.19.4/src/main/java/com/github/hummel/dirtequipment/handler/FabricEventHandler.java
@@ -13,18 +13,18 @@ public class FabricEventHandler {
 
 	public static void handle() {
 		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(populator -> {
-			populator.addAfter(DIRT_SWORD, new ItemStack(Items.NETHERITE_SWORD));
-			populator.addAfter(DIRT_HELMET, new ItemStack(Items.NETHERITE_BOOTS));
-			populator.addAfter(DIRT_CHESTPLATE, new ItemStack(Items.NETHERITE_BOOTS));
-			populator.addAfter(DIRT_LEGGINGS, new ItemStack(Items.NETHERITE_BOOTS));
-			populator.addAfter(DIRT_BOOTS, new ItemStack(Items.NETHERITE_BOOTS));
+			populator.addAfter(new ItemStack(Items.NETHERITE_SWORD), DIRT_SWORD);
+			populator.addAfter(new ItemStack(Items.NETHERITE_BOOTS), DIRT_HELMET);
+			populator.addAfter(new ItemStack(Items.NETHERITE_BOOTS), DIRT_CHESTPLATE);
+			populator.addAfter(new ItemStack(Items.NETHERITE_BOOTS), DIRT_LEGGINGS);
+			populator.addAfter(new ItemStack(Items.NETHERITE_BOOTS), DIRT_BOOTS);
 		});
 
 		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(populator -> {
-			populator.addAfter(DIRT_HOE, new ItemStack(Items.NETHERITE_HOE));
-			populator.addAfter(DIRT_AXE, new ItemStack(Items.NETHERITE_HOE));
-			populator.addAfter(DIRT_PICKAXE, new ItemStack(Items.NETHERITE_HOE));
-			populator.addAfter(DIRT_SHOVEL, new ItemStack(Items.NETHERITE_HOE));
+			populator.addAfter(new ItemStack(Items.NETHERITE_HOE), DIRT_HOE);
+			populator.addAfter(new ItemStack(Items.NETHERITE_HOE), DIRT_AXE);
+			populator.addAfter(new ItemStack(Items.NETHERITE_HOE), DIRT_PICKAXE);
+			populator.addAfter(new ItemStack(Items.NETHERITE_HOE), DIRT_SHOVEL);
 		});
 	}
 }
