70fd9365e9475ff96ec986f7e4c059adc04d76fb
Hummel009 <hummel.turbamentum@gmail.com>
2024-05-13 11:19:38 +0300

    Update ItemDirtArmor.java

--- appForge/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtArmor.java
+++ appForge/src/main/java/com/github/hummel/dirtequipment/item/ItemDirtArmor.java
@@ -1,9 +1,11 @@
 package com.github.hummel.dirtequipment.item;
 
 import com.github.hummel.dirtequipment.init.ArmorMaterials;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
 
 public class ItemDirtArmor extends ArmorItem {
@@ -12,7 +14,7 @@ public class ItemDirtArmor extends ArmorItem {
 	}
 
 	@Override
-	public String getArmorTexture(ItemStack itemStack, Entity entity, EquipmentSlot equipmentSlot, String layer) {
-		return "dirtequipment:textures/models/armor/dirt_layer_" + (equipmentSlot == EquipmentSlot.LEGS ? 2 : 1) + ".png";
+	public ResourceLocation getArmorTexture(ItemStack itemStack, Entity entity, EquipmentSlot equipmentSlot, ArmorMaterial.Layer layer, boolean innerModel) {
+		return new ResourceLocation("dirtequipment:textures/models/armor/dirt_layer_" + (equipmentSlot == EquipmentSlot.LEGS ? 2 : 1) + ".png");
 	}
 }
